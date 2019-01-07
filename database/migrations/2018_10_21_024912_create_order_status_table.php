e28215ae8e3056d49d0c04f4bfc7a0d55ca12b97
wildanicardi <wildanicardi9@gmail.com>
2019-01-08 01:53:59 +0700

    perubahan migrations

--- database/migrations/2018_10_21_024912_create_order_status_table.php
+++ database/migrations/2018_10_21_024912_create_order_status_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateKeranjangTable extends Migration
+class CreateOrderStatusTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_11_25_031218_create_order_items_table.php
+++ database/migrations/2018_11_25_031218_create_order_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDataPengirimanTable extends Migration
+class CreateOrderItemsTable extends Migration
 {
     /**
      * Run the migrations.
